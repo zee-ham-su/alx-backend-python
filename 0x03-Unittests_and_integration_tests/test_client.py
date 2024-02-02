2fb147bebef5d17c96dc05b5cbe901569452d771
Hamza Sufian <hsufiian@yahoo.com>
2024-02-02 13:02:35 +0000

    Fix indentation in test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -37,7 +37,7 @@ class TestGithubOrgClient(unittest.TestCase):
         with patch(
                 "client.GithubOrgClient.org",
                 new_callable=PropertyMock,
-        ) as mock_org:
+                ) as mock_org:
             mock_org.return_value = {
                 'repos_url': "https://api.github.com/users/google/repos",
             }
