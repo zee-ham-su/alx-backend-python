f12f06bbd23c6a9d522303f30639cc9d91d59e27
zee-ham-su <hsufiian@yahoo.com>
2024-12-14 10:10:21 +0000

    Revert "Remove unused GitHub organization client and utility functions"
    This reverts commit d02aea2cc7043d5768f6b70274443e8eed0506b4.
    
    revert push

---
+++ 0x03-Unittests_and_integration_tests/README.md
@@ -0,0 +1,47 @@
+
+# 0x03 - Unittests and Integration Tests
+
+This directory contains the code and documentation for the 0x03 - Unittests and Integration Tests project.
+
+## Description
+
+In this project, we will be implementing unit tests and integration tests for our Python code. Unit tests are used to test individual units or components of our code in isolation, while integration tests are used to test the interaction between different components of our code.
+
+
+## Resources:
+- [unittest — Unit testing framework](https://intranet.alxswe.com/rltoken/a_AEObGK8jeqPtTPmm-gIA)
+- [unittest.mock — mock object library](https://intranet.alxswe.com/rltoken/PKetnACd7FfRiU8_kpe5EA)
+- [How to mock a readonly property with mock?](https://intranet.alxswe.com/rltoken/2ueVPK1kWZuz525FvZ1v2Q)
+- [parameterized](https://intranet.alxswe.com/rltoken/mI7qc3Y42aZ7GTlLXDxgEg)
+- [Memoization] (https://intranet.alxswe.com/rltoken/x83Hdr54q4Vax5xQ2Z3HSA)
+"""
+
+
+## Learning Objectives
+
+At the end of this project, you are expected to be able to explain to anyone, without the help of Google:
+
+- The difference between unit and integration tests.
+- Common testing patterns such as mocking, parametrizations and fixtures
+
+
+## Requirements
+
+- All your files will be interpreted/compiled on Ubuntu 18.04 LTS using python3 (version 3.7)
+- All your files should end with a new line
+- The first line of all your files should be exactly #!/usr/bin/env python3
+- A README.md file, at the root of the folder of the project, is mandatory
+- Your code should use the pycodestyle style (version 2.5)
+- All your files must be executable
+- All your modules should have a documentation (python3 -c 'print(__import__("my_module").__doc__)')
+- All your classes should have a documentation (python3 -c 'print(__import__("my_module").MyClass.__doc__)')
+- All your functions (inside and outside a class) should have a documentation (python3 -c 'print(__import__("my_module").my_function.__doc__)' and python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)')
+- A documentation is not a simple word, it’s a real sentence explaining what’s the purpose of the module, class or method (the length of it will be verified)
+- All your functions and coroutines must be type-annotated.
+
+
+
+## Required Files
+- utils.py ( download[https://intranet-projects-files.s3.amazonaws.com/webstack/utils.py])
+- client.py ( download[https://intranet-projects-files.s3.amazonaws.com/webstack/client.py])
+- fixtures.py ( download[https://intranet-projects-files.s3.amazonaws.com/webstack/fixtures.py])
---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python3
+""" client testing module
+"""
+
+import unittest
+from typing import Dict
+from unittest.mock import patch, MagicMock, PropertyMock, Mock
+from requests import HTTPError
+from parameterized import parameterized, parameterized_class
+from fixtures import TEST_PAYLOAD
+
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Tests the `GithubOrgClient` class."""
+
+    @parameterized.expand([
+        ("google", {'login': "google"}),
+        ("abc", {'login': "abc"}),
+    ])
+    @patch("client.get_json")
+    def test_org(self, org: str,
+                 expct_result: Dict, magic_mock: MagicMock) -> None:
+        """Tests the `org` method."""
+
+        magic_mock.return_value = MagicMock(return_value=expct_result)
+        org_client = GithubOrgClient(org)
+
+        result = org_client.org()
+
+        magic_mock.assert_called_once_with(
+            "https://api.github.com/orgs/{}".format(org))
+
+        self.assertEqual(result, expct_result)
+
+    def test_public_repos_url(self) -> None:
+        """Tests the `_public_repos_url` property."""
+        with patch(
+                "client.GithubOrgClient.org",
+                new_callable=PropertyMock,
+        ) as mock_org:
+            mock_org.return_value = {
+                'repos_url': "https://api.github.com/users/google/repos",
+            }
+            org_client = GithubOrgClient("google")
+
+            self.assertEqual(
+                org_client._public_repos_url,
+                "https://api.github.com/users/google/repos",
+            )
+
+    @patch("client.get_json")
+    def test_public_repos(self, mock_get_json: MagicMock) -> None:
+        """Tests the `public_repos` method."""
+        test_payload = {
+            'repos_url': "https://api.github.com/users/google/repos",
+            'repos': [
+                {
+                    "id": 1,
+                    "name": "Hamza",
+                    "private": False,
+                    "owner": {
+                        "login": "google",
+                        "id": 1,
+                    },
+                    "fork": False,
+                    "url": "https://api.github.com/repos/google/hamza",
+                    "html_url": "https://github.com/google/hamza",
+                    "created_at": "2024-01-10T00:00:00Z",
+                    "updated_at": "2024-02-10T00:00:00Z",
+                    "has_issues": True,
+                    "forks": 10,
+                    "default_branch": "master",
+                },
+                {
+                    "id": 2,
+                    "name": "abc",
+                    "private": False,
+                    "owner": {
+                        "login": "abc",
+                        "id": 2,
+                    },
+                    "fork": False,
+                    "url": "https://api.github.com/repos/google/abc",
+                    "html_url": "https://github.com/google/abc",
+                    "created_at": "2024-01-10T00:00:00Z",
+                    "updated_at": "2024-02-10T00:00:00Z",
+                    "has_issues": True,
+                    "forks": 10,
+                    "default_branch": "master",
+                },
+            ]
+        }
+        mock_get_json.return_value = test_payload["repos"]
+        with patch(
+            "client.GithubOrgClient._public_repos_url",
+            new_callable=PropertyMock,
+        ) as mock_public_repos_url:
+            mock_public_repos_url.return_value = test_payload["repos_url"]
+            result = GithubOrgClient("google").public_repos()
+            self.assertEqual(result, ["Hamza", "abc"])
+            mock_public_repos_url.assert_called_once()
+        mock_get_json.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expct_result) -> None:
+        """Tests the `has_license` method."""
+        org_client = GithubOrgClient("google")
+        result = org_client.has_license(repo, license_key)
+        self.assertEqual(result, expct_result)
+
+
+@parameterized_class([
+    {
+        'org_payload': TEST_PAYLOAD[0][0],
+        'repos_payload': TEST_PAYLOAD[0][1],
+        'expected_repos': TEST_PAYLOAD[0][2],
+        'apache2_repos': TEST_PAYLOAD[0][3],
+    },
+])
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """Integration tests for the `GithubOrgClient` class."""
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        """Sets up the class fixtures."""
+        cls.get_patcher = patch('requests.get')
+        cls.mock_get = cls.get_patcher.start()
+
+        def side_effect(url):
+            """Side effect function for mocking requests.get"""
+            route_payload = {
+                'https://api.github.com/orgs/google': cls.org_payload,
+                'https://api.github.com/orgs/google/repos': cls.repos_payload,
+            }
+            if url in route_payload:
+                return Mock(**{'json.return_value': route_payload[url]})
+            return Mock(**{'json.return_value': {}})
+
+        cls.mock_get.side_effect = side_effect
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        """Tears down the class fixtures."""
+        cls.get_patcher.stop()
+
+    def test_public_repos(self) -> None:
+        """Tests the `public_repos` method."""
+        client = GithubOrgClient("google")
+        self.assertEqual(client.public_repos(), self.expected_repos)
+
+    def test_public_repos_with_license(self) -> None:
+        """Tests the `public_repos` method with a license."""
+        client = GithubOrgClient("google")
+        self.assertEqual(client.public_repos(license="apache-2.0"), self.apache2_repos)
