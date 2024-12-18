0e3c47b4a115619c5779d8cd301ebba241191149
zee-hamm-su <hsufiian@yahoo.com>
2024-12-18 19:09:55 +0000

    Implement viewsets for Conversation and Message models; add create functionality and filter by user participation

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer, ConversationCreateSerializer, MessageCreateSerializer
 
-# Create your views here.
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+
+    def get_queryset(self):
+        return Conversation.objects.filter(participants=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return ConversationCreateSerializer
+        return ConversationSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        conversation = serializer.save()
+        conversation.participants.add(request.user)
+        headers = self.get_success_headers(serializer.data)
+        return Response(ConversationSerializer(conversation).data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+    def get_queryset(self):
+        return Message.objects.filter(conversation__participants=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return MessageCreateSerializer
+        return MessageSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(sender=request.user)
+        headers = self.get_success_headers(serializer.data)
+        return Response(MessageSerializer(serializer.instance).data, status=status.HTTP_201_CREATED, headers=headers)
