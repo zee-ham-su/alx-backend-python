d998d7aabc5669647aa642e1f672a53f914dc5a6
zee-hamm-su <hsufiian@yahoo.com>
2024-12-18 16:37:34 +0000

    Add REST framework authentication and permission settings in settings.py

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -83,6 +83,16 @@ DATABASES = {
 }
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
