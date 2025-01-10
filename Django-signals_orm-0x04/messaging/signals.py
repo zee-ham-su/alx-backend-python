5b667379ac924615767fc644f19211c382f22a6c
Hamza Sufian <111157318+zee-ham-su@users.noreply.github.com>
2025-01-10 11:33:38 +0000

    Delete Django-signals_orm-0x04/messaging directory

--- Django-signals_orm-0x04/messaging/signals.py
+++
@@ -1,8 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from .models import Message, Notification
-
-@receiver(post_save, sender=Message)
-def create_notification(sender, instance, created, **kwargs):
-    if created:
-        Notification.objects.create(user=instance.receiver, message=instance)
