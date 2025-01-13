9c61aa5f77fe433e3267b88a736db3d399e26f58
zee-ham-su <hsufiian@yahoo.com>
2025-01-13 19:30:17 +0000

    Added RestrictAccessByTimeMiddleware to restrict access to messaging app between 9 PM and 6 AM

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,6 +1,7 @@
 import logging
-from datetime import datetime
+from datetime import datetime, time
 from django.utils.deprecation import MiddlewareMixin
+from django.http import HttpResponseForbidden
 
 
 logger = logging.getLogger(__name__)
@@ -16,6 +17,24 @@ class RequestLoggingMiddleware(MiddlewareMixin):
 
         with open("requests.log", "a") as log_file:
             log_file.write(log_message + "\n")
+
+        response = self.get_response(request)
+        return response
+
+
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        start_time = time(21, 0)
+        end_time = time(6, 0)
+
+        from datetime import datetime
+        now = datetime.now().time()
+
+        if start_time <= now or now <= end_time:
+            return HttpResponseForbidden("Access to the messaging app is restricted between 9 PM and 6 AM.")
 
         response = self.get_response(request)
         return response
--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'chats.middleware.RequestLoggingMiddleware',
+    'chats.middleware.RestrictAccessByTimeMiddleware', 
 ]
 
 ROOT_URLCONF = 'messaging_app.urls'
