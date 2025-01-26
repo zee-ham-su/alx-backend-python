72d89735ca09490f4f5ac8288c9e21efc198fcd5
zee-ham-su <hsufiian@yahoo.com>
2025-01-26 16:59:37 +0000

    fix: update database engine to MySQL and expand requirements for compatibility

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv('DATABASE_NAME'),
         'USER': os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
